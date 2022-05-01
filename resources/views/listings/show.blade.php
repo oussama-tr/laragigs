eaf202e1e1e74624ad074386cce3847a03af65f9
Oussama Trabelsi <oussamatrabelsi@Oussamas-MacBook-Pro.local>
2022-05-01 14:01:27 +0100

    edit listing

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -7,6 +7,7 @@
                 <img class="w-48 mr-6 mb-6"
                      src="{{$listing->logo ? asset('storage/' . $listing->logo) : asset('/images/no-image.png')}}"
                      alt=""/>
+
                 <h3 class="text-2xl mb-2">
                     {{$listing->title}}
                 </h3>
@@ -24,7 +25,7 @@
                         {{$listing->description}}
 
                         <a href="mailto:{{$listing->email}}"
-                           class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"><i
+                           class="block bg-primary text-white mt-6 py-2 rounded-xl hover:opacity-80"><i
                                     class="fa-solid fa-envelope"></i>
                             Contact Employer</a>
 
@@ -35,6 +36,17 @@
                     </div>
                 </div>
             </div>
+        </x-card>
+
+        <x-card class="mt-4 p-2 flex space-x-6">
+            <a href="/listings/{{$listing->id}}/edit">
+                <i class="fa-solid fa-pencil"></i> Edit
+            </a>
+            <form method="POST" action="/listings/{{$listing->id}}">
+                @csrf
+                @method('DELETE')
+                <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
+            </form>
         </x-card>
     </div>
 </x-layout>
